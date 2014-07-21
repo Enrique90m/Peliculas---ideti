5501cd81e74fff47b4e4891bc7fc88699002f07d
rafael <rafa.eac@gmail.com>
2014-07-21 17:03:30 -0500

    Adecuaciones clase Pelicula
    Agrego a la clase pelicula sus variables
    Cosntructor vacio
    y constructor que recibe todos los parametros
    asi como los getters and setters

--- Peliculas-Ideti/Peliculas-Ideti/Pelicula.cs
+++ Peliculas-Ideti/Peliculas-Ideti/Pelicula.cs
@@ -7,5 +7,51 @@ namespace Peliculas_Ideti
 {
     class Pelicula
     {
+        private String titulo;
+        private String genero;
+        private int anio;
+
+        public Pelicula() 
+        {
+
+        }
+
+        public Pelicula(String titulo, String genero, int anio) 
+        {
+            this.titulo = titulo;
+            this.genero = genero;
+            this.anio = anio;
+        }
+
+        public String getTitulo() 
+        {
+            return titulo;
+        }
+
+        public String getGenero() 
+        {
+            return genero;
+        }
+
+        public int getAnio() 
+        {
+            return anio;
+        }
+
+        public void setTitulo(String titulo )
+        {
+            this.titulo = titulo;
+        }
+
+        public void setGenero(String genero) 
+        {
+            this.genero = genero;
+        }
+
+        public void setAnio(int anio) 
+        {
+            this.anio = anio;
+        }
+
     }
 }
